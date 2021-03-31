eb2ea85dd9201759a74b494a7d8a2884f80469c7
Abhishek Upadhyay <akrupadhyay.met18@itbhu.ac.in>
2021-03-31 15:46:44 +0530

    updated models, routes

--- server/index.js
+++ server/index.js
@@ -1,4 +1,25 @@
 import express from 'express';
-import bodyparser from 'body-parser';
+import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+
+import postRoutes from './routes/posts.js';
+
+const app=express();
+
+app.use('/posts',postRoutes);
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL='mongodb+srv://abhishek:abhishek123@cluster0.ksl0t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT=process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
+    .then(()=>app.listen(PORT,()=> console.log(`Server running on PORT: ${PORT}`)))
+    .catch((err)=> console.log(err.message));
+
+mongoose.set('useFindAndModify',false);
+
+
