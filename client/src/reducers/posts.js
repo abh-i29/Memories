68310f2c30755cb1609aa3476f653e4e5e91b1a3
Abhishek Upadhyay <akrupadhyay.met18@itbhu.ac.in>
2021-04-03 17:23:02 +0530

    Updated .env

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,3 +1,4 @@
+import { FETCH_ALL,CREATE,UPDATE,DELETE,LIKE } from '../constants/actionTypes';
 
 // here posts array is the state of posts and reducers are
 // used to perfom some action according to their type
@@ -8,13 +9,15 @@
 export default (posts=[], action)=>{
     switch(action.type)
     {
-        case 'UPDATE':
+        case UPDATE:
             return posts.map((post) => post._id === action.payload._id ? action.payload : post) //action.payload is updated post
-        case 'DELETE':
+        case LIKE:
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post) 
+        case DELETE:
             return posts.filter((post) => post._id !== action.payload); //action.payload is id which we do not want
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload; //posts
-        case 'CREATE':
+        case CREATE:
             return [...posts, action.payload];
         default:
             return posts;
--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app=express();
+dotenv.config();
 
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
@@ -14,10 +16,10 @@ app.use(cors());
 
 app.use('/posts',postRoutes);
 
-const CONNECTION_URL='mongodb+srv://abhishek:abhishek123@cluster0.ksl0t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const PORT=process.env.PORT || 5000;
+// const CONNECTION_URL='MONGO_URL'
+const PORT=process.env.PORT;
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
     .then(()=>app.listen(PORT,()=> console.log(`Server running on PORT: ${PORT}`)))
     .catch((err)=> console.log(err.message));
 
